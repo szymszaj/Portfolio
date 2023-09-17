9ad1591288ad0ae3c6e1b5cf1206c115588e83ab
Szymon Zych <szymonzych868@gmail.com>
2023-09-17 22:09:31 +0200

    adding smooth animation to page scrolling

--- portfolio/app.js
+++ portfolio/app.js
@@ -32,3 +32,20 @@ let typed = new Typed('.auto-input', {
 
  // Ustaw interwał do sprawdzania zmiany roku co minutę
  setInterval(updateYear, 60000); // Możesz dostosować częstotliwość do swoich potrzeb
+
+
+
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
