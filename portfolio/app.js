e044e31139ed8c56626168b3e9e5d55b14245638
Szymon Zych <szymonzych868@gmail.com>
2023-09-17 22:35:44 +0200

    Navigation disappears and reappears when the user presses a button

--- portfolio/app.js
+++ portfolio/app.js
@@ -34,7 +34,7 @@ let typed = new Typed('.auto-input', {
  setInterval(updateYear, 60000); // Możesz dostosować częstotliwość do swoich potrzeb
 
 
-
+//plynne przewijanie po nacisnięciu na odnosnik w nawigacji 
  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -49,3 +49,63 @@ let typed = new Typed('.auto-input', {
     }
   });
 });
+
+//zamykanie odnosnika po nacisnieciu 
+function closeNavAndFooter() {
+  const nav = document.querySelector("header");
+  const footer = document.querySelector(".footer");
+  
+  // Ukryj nawigację
+  nav.style.left = "-300px";
+  
+  // Ukryj stopkę
+  footer.style.bottom = "-80px"; // Możesz dostosować wartość bottom do preferencji
+}
+
+function openNavAndFooter() {
+  const nav = document.querySelector("header");
+  const footer = document.querySelector(".footer");
+  
+  // Po kliknięciu w przycisk MenuBtn, pokaż nawigację i stopkę
+  nav.style.left = "0";
+  footer.style.bottom = "0";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("header nav ul li a");
+  const menuBtn = document.getElementById("MenuBtn");
+  
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth <= 992) {
+        // Wywołaj funkcję closeNavAndFooter() z opóźnieniem, aby zsynchronizować zamykanie nawigacji i stopki
+        setTimeout(closeNavAndFooter, 400); // 400 ms (możesz dostosować czas do swoich preferencji)
+      }
+    });
+  });
+
+  menuBtn.addEventListener("click", () => {
+    if (window.innerWidth <= 992) {
+      openNavAndFooter();
+    } else {
+      closeNavAndFooter();
+    }
+  });
+
+  // Obsługa zdarzenia resize
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 992) {
+      // Jeśli szerokość ekranu jest większa niż 992px, pokaż nawigację i stopkę
+      openNavAndFooter();
+    } else {
+      // W przeciwnym razie ukryj nawigację i stopkę
+      closeNavAndFooter();
+    }
+  });
+
+  // Wywołaj funkcję closeNavAndFooter() przy załadowaniu strony, jeśli szerokość ekranu jest mniejsza lub równa 992px
+  if (window.innerWidth <= 992) {
+    closeNavAndFooter();
+  }
+});
+
