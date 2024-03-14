d5ef1eb51b8ca3bd1af37c1e07a4522ab330da4e
Szymon Zych <szymonzych868@gmail.com>
2024-03-14 19:59:57 +0100

    Added automatic age calculation on the contact page

--- portfolio/app.js
+++ portfolio/app.js
@@ -91,3 +91,16 @@ document.addEventListener("DOMContentLoaded", function () {
     closeNavAndFooter();
   }
 });
+const ageElement = document.getElementById("age");
+
+// 
+function calculateAge(birthDate) {
+  const currentDate = new Date();
+  const diff = currentDate - birthDate;
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+const birthDate = new Date("2003-05-16");
+
+ageElement.textContent = calculateAge(birthDate);
