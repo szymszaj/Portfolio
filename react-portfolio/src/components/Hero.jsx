6cade247a654840d9ccfbb78fb9b54324323aa01
Szymon Zych <szymonzych868@gmail.com>
2024-06-13 21:34:06 +0200

    creating a home page

---
+++ react-portfolio/src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/profile.jpg";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Szymon Zych
+            </h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              Junior Front-end Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/3 lg:p-8">
+          <div className="flex justify-center">
+            <img className="rounded-lg" src={profilePic} alt="Szymon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
