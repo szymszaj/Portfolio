a6015a2396c494e1483b354c490218c251f2a08d
Szymon Zych <szymonzych868@gmail.com>
2024-06-13 21:37:33 +0200

    adding constants

---
+++ react-portfolio/src/constants/index.js
@@ -0,0 +1,6 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Jestem Junior Front-end Developerem z pasją do tworzenia nowoczesnych i responsywnych aplikacji internetowych. Przez ostatnie dwa lata samodzielnie doskonaliłem swoje umiejętności, zdobywając wiedzę w zakresie technologii front-endowych. W pracy rozwijam się poprzez tworzenie projektów e-commerce, gdzie na co dzień korzystam z React, Tailwind CSS i JavaScript. Moim celem jest dalszy rozwój w dziedzinie front-endu oraz tworzenie intuicyjnych i wydajnych rozwiązań, które dostarczają użytkownikom wyjątkowe doświadczenia.`;
