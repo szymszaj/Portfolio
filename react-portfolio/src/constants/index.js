00a4426f942362335a4a0d16f3101cbafbe3e9f7
Szymon Zych <szymonzych868@gmail.com>
2024-06-16 19:26:44 +0200

    adding EXPERIENCES

--- react-portfolio/src/constants/index.js
+++ react-portfolio/src/constants/index.js
@@ -6,3 +6,19 @@ import project4 from "../assets/projects/project-4.jpg";
 export const HERO_CONTENT = `Jestem Junior Front-end Developerem z pasją do tworzenia nowoczesnych i responsywnych aplikacji internetowych. Przez ostatnie dwa lata samodzielnie doskonaliłem swoje umiejętności, zdobywając wiedzę w zakresie technologii front-endowych. W pracy rozwijam się poprzez tworzenie projektów e-commerce, gdzie na co dzień korzystam z React, Tailwind CSS i JavaScript. Moim celem jest dalszy rozwój w dziedzinie front-endu oraz tworzenie intuicyjnych i wydajnych rozwiązań, które dostarczają użytkownikom wyjątkowe doświadczenia.`;
 
 export const ABOUT_TEXT = `Jestem pasjonatem technologii i entuzjastą rozwoju osobistego. Moje zainteresowanie dziedziną informatyki narodziło się z fascynacji możliwościami, jakie niesie ze sobą świat cyfrowy. Moim głównym celem zawodowym jest stać się wykwalifikowanym programistą, zdolnym do tworzenia innowacyjnych rozwiązań oraz rozwijania aplikacji, które mają wpływ na ludzi i otaczający nas świat. Jednym z moich najważniejszych atutów jest pasja do nauki nowych rzeczy. Uwielbiam wyzwania, które stawiają przede mną technologie i rozwiązania informatyczne, i jestem gotów stale poszerzać swoją wiedzę oraz umiejętności. Wierzę, że umiejętność ciągłego uczenia się jest kluczowa w dzisiejszym dynamicznym świecie technologii. Ponadto, moje zainteresowania w życiu prywatnym również pokazują moją pasję do technologii. Uwielbiam rozwiązywać zagadki i wyzwania, dlatego też często spędzam czas nad projektami programistycznymi poza pracą. Gra w gry komputerowe i sport stanowią dla mnie doskonałe narzędzia do rozwijania umiejętności zręcznościowych, koncentracji i współpracy zespołowej, które są równie istotne w świecie technologii.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Junior Front-end Developer",
+    company: "Monogo",
+    description: `Tworzenie sklepu internetowego przy użyciu React.js i Tailwind CSS. Obsługa panelu Magento 2. Dodawanie data-testID do projektu oraz tworzenie i wdrażanie testów automatyzujących dla bloga na stronie sklepu. Współpraca z zespołem projektowym w celu określenia wymagań. Rozwój gita, React.js oraz JavaScript w trakcie realizacji projektu. `,
+    technologies: [
+      "Javascript",
+      "React.js",
+      "HTML",
+      "Tailwindcss",
+      "Playwright",
+    ],
+  },
+];
