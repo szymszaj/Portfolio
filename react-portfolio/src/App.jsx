a951c9abed4bb078dcd58c0c1f785e6b69dfc73e
Szymon Zych <szymonzych868@gmail.com>
2024-06-17 11:54:43 +0200

    adding imports

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
+        <Contact />
       </div>
     </div>
   );
