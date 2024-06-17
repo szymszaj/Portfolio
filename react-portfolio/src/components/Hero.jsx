9189e848fa3127ffcc15fb8ef756c39f06221a58
Szymon Zych <szymonzych868@gmail.com>
2024-06-17 11:54:07 +0200

    adding a new function to animation

--- react-portfolio/src/components/Hero.jsx
+++ react-portfolio/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { HERO_CONTENT } from "../constants";
 import VSCODE from "../assets/3.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -7,12 +8,17 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              initial={{ x: -100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Szymon Zych
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Junior Front-end Developer
-            </span>
+            </motion.span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {HERO_CONTENT}
             </p>
