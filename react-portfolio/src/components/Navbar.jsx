74186825ec60379ad98478d075468b7a142455f1
Szymon Zych <szymonzych868@gmail.com>
2024-11-20 20:42:48 +0100

    refactor: Replace individual social media links with a mapped array for Navbar component

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
-import { FaSquareTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
@@ -11,41 +10,26 @@ const Navbar = () => {
         <span className="text-3xl font-bold text-white">SH</span>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a
-          href="https://www.linkedin.com/in/szymon-zych-5b2950296/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://github.com/szymszaj"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://www.facebook.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaFacebook />
-        </a>
-        <a
-          href="https://x.com/SzymonZych12"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaSquareTwitter />
-        </a>
-        <a
-          href="https://www.instagram.com/szymszaj/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram />
-        </a>
+        {[
+          {
+            href: "https://www.linkedin.com/in/szymon-zych-5b2950296/",
+            icon: FaLinkedin,
+          },
+          { href: "https://github.com/szymszaj", icon: FaGithub },
+          { href: "https://www.facebook.com", icon: FaFacebook },
+          { href: "https://x.com/SzymonZych12", icon: FaXTwitter },
+        ].map(({ href, icon: Icon }, index) => (
+          <a
+            key={index}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group relative text-whitetransition-colors duration-300"
+          >
+            <Icon />
+            <span className="absolute left-0 bottom-[-8px] h-[2px] w-0 bg-current transition-all duration-300 group-hover:w-full"></span>
+          </a>
+        ))}
       </div>
     </nav>
   );
