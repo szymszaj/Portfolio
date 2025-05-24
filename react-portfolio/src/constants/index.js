edda722fe607dd24fbfb6f07d5a5ed152c4a846c
Szymon Zych <szymonzych868@gmail.com>
2025-05-24 22:59:55 +0200

    refactor: Update HERO_CONTENT and EXPERIENCES with new role and company details

--- react-portfolio/src/constants/index.js
+++ react-portfolio/src/constants/index.js
@@ -3,13 +3,27 @@ import project2 from "../assets/projects/game.png";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `Jestem Junior Front-end Developerem z pasją do tworzenia nowoczesnych i responsywnych aplikacji internetowych. Przez ostatnie trzy lata samodzielnie doskonaliłem swoje umiejętności, zdobywając wiedzę w zakresie technologii front-endowych. W pracy rozwijam się poprzez tworzenie projektów e-commerce, gdzie na co dzień korzystam z React, Next.js oraz Javascript. Moim celem jest dalszy rozwój w dziedzinie front-endu oraz tworzenie intuicyjnych i wydajnych rozwiązań.`;
+export const HERO_CONTENT = `Jestem  Front-end Developerem z pasją do tworzenia nowoczesnych i responsywnych aplikacji internetowych. Przez ostatnie trzy lata samodzielnie doskonaliłem swoje umiejętności, zdobywając wiedzę w zakresie technologii front-endowych. W pracy rozwijam się poprzez tworzenie projektów e-commerce, gdzie na co dzień korzystam z React, Next.js oraz Javascript. Moim celem jest dalszy rozwój w dziedzinie front-endu oraz tworzenie intuicyjnych i wydajnych rozwiązań.`;
 
 export const ABOUT_TEXT = `Jestem pasjonatem technologii i entuzjastą rozwoju osobistego. Moje zainteresowanie dziedziną informatyki narodziło się z fascynacji możliwościami, jakie niesie ze sobą świat cyfrowy. Moim głównym celem zawodowym jest stać się wykwalifikowanym programistą, zdolnym do tworzenia innowacyjnych rozwiązań oraz rozwijania aplikacji, które mają wpływ na ludzi i otaczający nas świat. Jednym z moich najważniejszych atutów jest pasja do nauki nowych rzeczy. Uwielbiam wyzwania, które stawiają przede mną technologie i rozwiązania informatyczne, i jestem gotów stale poszerzać swoją wiedzę oraz umiejętności. Wierzę, że umiejętność ciągłego uczenia się jest kluczowa w dzisiejszym dynamicznym świecie technologii. Ponadto, moje zainteresowania w życiu prywatnym również pokazują moją pasję do technologii. Uwielbiam rozwiązywać zagadki i wyzwania, dlatego też często spędzam czas nad projektami programistycznymi poza pracą. Gra w gry komputerowe i sport stanowią dla mnie doskonałe narzędzia do rozwijania umiejętności zręcznościowych, koncentracji i współpracy zespołowej, które są równie istotne w świecie technologii.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
+    year: "2025 - Present",
+    role: "Junior Front-end Developer",
+    company: "Indigital",
+    description: `Tworzenie stron internetowych i aplikacji z wykorzystaniem technologii React.js oraz Next.js. Praca z systemem zarządzania treścią WordPress, w tym tworzenie i modyfikacja motywów. Udział w projektach e-commerce, gdzie odpowiedzialność obejmowała integrację z API oraz optymalizację wydajności aplikacji.`,
+    technologies: [
+      "Javascript",
+      "React.js",
+      "Tailwind CSS",
+      "Next.js",
+      "WordPress",
+    ],
+  },
+
+  {
+    year: "2023 - 2025",
     role: "Junior Front-end Developer",
     company: "Monogo",
     description: `Tworzenie sklepu internetowego z wykorzystaniem technologii React.js oraz Tailwind CSS. Tworzenie nowoczesnych aplikacji z wykorzystaniem Next.js oraz integracja z Builder.io, co pozwala na dynamiczne zarządzanie treścią i szybsze wdrażanie nowych funkcji. Tworzenie i wdrażanie testów jednostkowych dla aplikacji klienckiej. Automatyzacja testów dla bloga na stronie sklepu. Aktywny rozwój umiejętności w zakresie Git, React.js oraz JavaScript, co przyczynia się do optymalizacji procesów i podniesienia jakości kodu.`,
