6402b04fb40760826b0fcfd276d840b49218ba45
Szymon Zych <szymonzych868@gmail.com>
2025-10-16 22:46:47 +0200

    feat: Uncomment Projects component in App for improved visibility

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 import { Contact } from "./components/Contact";
 // import ParticleBackground from "./components/ui/ParticleBackground";
 
@@ -22,7 +22,7 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
-        {/* <Projects /> */}
+        <Projects />
         <Contact />
       </div>
     </div>
